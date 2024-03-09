df1eedb87f264b6642264a0323b5fd8557e04272
Klif <sang.wicklife@student.moringaschool.com>
2024-03-09 18:17:31 +0300

    3 failed

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='John Doe', job='General Management'):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        """The name property"""
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        """Name must be a string between 1 and 25 characters in length"""
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()  # Convert name to title case
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        """The job property"""
+        return self._job
+
+    @job.setter
+    def job(self, job):
+        """Job must be in the list of approved jobs"""
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
